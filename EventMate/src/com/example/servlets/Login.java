37efb4ce2e2ed06456c7b15963db533c51757eb0
karivmcmahon <kmc_xo@hotmail.co.uk>
2014-03-28 20:10:01 +0000

    Re-added Steph's security code in

--- EventMate/src/com/example/servlets/Login.java
+++ EventMate/src/com/example/servlets/Login.java
@@ -1,6 +1,8 @@
 package com.example.servlets;
 
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
@@ -22,25 +24,27 @@ import com.example.stores.UserStore;
 @WebServlet(urlPatterns = { "/Login", "/Login/*" })
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private Cluster cluster; 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-    
-    public void init(ServletConfig config) throws ServletException {
+	private Cluster cluster;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void init(ServletConfig config) throws ServletException {
 		// TODO Auto-generated method stub
 		cluster = CassandraHosts.getCluster();
 	}
 
 	/**
-	 * Servlet checks if user is valid and either directs to Event page or back to sign up page
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		UserStore us = new UserStore();
 		UserModel um = new UserModel();
@@ -67,41 +71,94 @@ public class Login extends HttpServlet {
 	}
 
 	/**
-	 * Servlet checks if user is valid and either directs to Event page or back to sign up page
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
 		UserModel um = new UserModel();
 		UserStore us = new UserStore();
 		um.setCluster(cluster);
 		us.setUsername(request.getParameter("username"));
-		us.setPassword(request.getParameter("password"));
-		try
-		{
+		String password = "?btY?N1&zt" + request.getParameter("password") + "4IoIS^NY!r";
+		String pw = "";
+		try {
+			pw = encrypt(password);
+		} catch (NoSuchAlgorithmException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		us.setPassword(pw);
+		try {
 			us = um.getUser(us);
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 
 		}
 
-		if(us.getValid())
-		{
+		if (us.getValid()) {
 			us.setLoggedIn(true);
 			HttpSession session = request.getSession(true);
 			session.setAttribute("currentSeshUser", us);
-			//Direct to home.jsp once session true
+			// Direct to home.jsp once session true
 			response.sendRedirect("/EventMate/Event");
-		}
-		else
-		{
+		} else {
 			System.out.println("not valid");
 			us.setLoggedIn(false);
-			RequestDispatcher rd = request.getRequestDispatcher("/Home.jsp"); 
-			request.setAttribute("invalidLogin", "incorrect username or password");
-		    rd.forward(request, response); 
-		}
+			RequestDispatcher rd = request.getRequestDispatcher("/Home.jsp");
+			request.setAttribute("invalidLogin",
+					"incorrect username or password");
+			rd.forward(request, response);
 		}
 	}
+
+	/**
+	 * Password encryption
+	 * @param pass
+	 * @return
+	 * @throws NoSuchAlgorithmException
+	 */
+	public String encrypt(String pass) throws NoSuchAlgorithmException {
+		// Set password string, and print it out
+		String passwd = pass;
+		System.out.println("Password is: " + passwd + ".<br>");
+
+		// Create a new instance of MessageDigest, using MD5. SHA and other
+		// digest algorithms are also available.
+		MessageDigest alg = MessageDigest.getInstance("MD5");
+
+		// Reset the digest, in case it's been used already during this section
+		// of code
+		// This probably isn't needed for pages of 210 simplicity
+		alg.reset();
+
+		// Calculate the md5 hash for the password. md5 operates on bytes, so
+		// give
+		// MessageDigest the byte verison of the string
+		alg.update(passwd.getBytes());
+
+		// Create a byte array from the string digest
+		byte[] digest = alg.digest();
+
+		// Convert the hash from whatever format it's in, to hex format
+		// which is the normal way to display and report md5 sums
+		// This is done byte by byte, and put into a StringBuffer
+		StringBuffer hashedpasswd = new StringBuffer();
+		String hx;
+		for (int i = 0; i < digest.length; i++) {
+			hx = Integer.toHexString(0xFF & digest[i]);
+			// 0x03 is equal to 0x3, but we need 0x03 for our md5sum
+			if (hx.length() == 1) {
+				hx = "0" + hx;
+			}
+			hashedpasswd.append(hx);
+		}
+
+		// Print out the string hex version of the md5 hash
+		System.out.println("MD5 version is: " + hashedpasswd.toString()
+				+ "<br>");
+		return hashedpasswd.toString();
+
+	}
+}
--- EventMate/src/com/example/servlets/SignUp.java
+++ EventMate/src/com/example/servlets/SignUp.java
@@ -1,6 +1,8 @@
 package com.example.servlets;
 
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
@@ -27,71 +29,123 @@ import com.example.stores.UserStore;
 public class SignUp extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private Cluster cluster;
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SignUp() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-    
-    public void init(ServletConfig config) throws ServletException {
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SignUp() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void init(ServletConfig config) throws ServletException {
 		// TODO Auto-generated method stub
 		cluster = CassandraHosts.getCluster();
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
 	/**
-	 * This servlet sends gets user information from Home.jsp file and sets it to a user store
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		UserModel um = new UserModel();
 		um.setCluster(cluster);
 		UserStore us = new UserStore();
 		UserStore user = new UserStore();
-		//If username,password or email null then display error
-		if(request.getParameter("usernameSignUp").equals("") || request.getParameter("passwordSignUp").equals("") || request.getParameter("emailSignUp").equals(""))
-		{
+		if (request.getParameter("usernameSignUp").equals("")
+				|| request.getParameter("passwordSignUp").equals("")
+				|| request.getParameter("emailSignUp").equals("")) {
 			RequestDispatcher rd = request.getRequestDispatcher("/Home.jsp");
 			request.setAttribute("invalidSignUp", "Invalid");
 			rd.forward(request, response);
-		}
-		else
-		{
-			//Set username,password,email
+		} else {
 			us.setUsername(request.getParameter("usernameSignUp"));
-			us.setPassword(request.getParameter("passwordSignUp"));
-			us.setEmail(request.getParameter("emailSignUp"));
-			//If check username does not return null display error
-			if (um.checkUsername(us) != "") 
-			{
-				RequestDispatcher rd = request.getRequestDispatcher("/Home.jsp");
-				request.setAttribute("invalidSignUp", "Username already in use!");
-				rd.forward(request, response);
+			String password = "?btY?N1&zt" + request.getParameter("passwordSignUp") + "4IoIS^NY!r";
+			System.out.println("password" + password);
+
+			String pw = "";
+			try {
+				pw = encrypt(password);
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			//If check email does not return null display error
-			else if(um.checkEmail(us) != "")
-			{
-				RequestDispatcher rd = request.getRequestDispatcher("/Home.jsp");
+			us.setPassword(pw);
+			us.setEmail(request.getParameter("emailSignUp"));
+			if (um.checkUsername(us) != "") {
+				RequestDispatcher rd = request
+						.getRequestDispatcher("/Home.jsp");
+				request.setAttribute("invalidSignUp",
+						"Username already in use!");
+				rd.forward(request, response);
+			} else if (um.checkEmail(us) != "") {
+				RequestDispatcher rd = request
+						.getRequestDispatcher("/Home.jsp");
 				request.setAttribute("invalidSignUp", "Email already in use!");
 				rd.forward(request, response);
-			}else{
-				//Set user store to the session possibleUser and direct to next form
+			} else {
 				HttpSession session = request.getSession(true);
 				session.setAttribute("possibleUser", us);
-				us = (UserStore) request.getSession().getAttribute("possibleUser");
+				us = (UserStore) request.getSession().getAttribute(
+						"possibleUser");
 				// Direct to home.jsp once session true
 				response.sendRedirect("/EventMate/Bio");
 			}
 		}
+	}
+
+	public String encrypt(String pass) throws NoSuchAlgorithmException {
+		// Set password string, and print it out
+		String passwd = pass;
+		System.out.println("Password is: " + passwd + ".<br>");
+
+		// Create a new instance of MessageDigest, using MD5. SHA and other
+		// digest algorithms are also available.
+		MessageDigest alg = MessageDigest.getInstance("MD5");
+
+		// Reset the digest, in case it's been used already during this section
+		// of code
+		// This probably isn't needed for pages of 210 simplicity
+		alg.reset();
+
+		// Calculate the md5 hash for the password. md5 operates on bytes, so
+		// give
+		// MessageDigest the byte verison of the string
+		alg.update(passwd.getBytes());
+
+		// Create a byte array from the string digest
+		byte[] digest = alg.digest();
+
+		// Convert the hash from whatever format it's in, to hex format
+		// which is the normal way to display and report md5 sums
+		// This is done byte by byte, and put into a StringBuffer
+		StringBuffer hashedpasswd = new StringBuffer();
+		String hx;
+		for (int i = 0; i < digest.length; i++) {
+			hx = Integer.toHexString(0xFF & digest[i]);
+			// 0x03 is equal to 0x3, but we need 0x03 for our md5sum
+			if (hx.length() == 1) {
+				hx = "0" + hx;
+			}
+			hashedpasswd.append(hx);
+		}
+
+		// Print out the string hex version of the md5 hash
+		System.out.println("MD5 version is: " + hashedpasswd.toString()
+				+ "<br>");
+		return hashedpasswd.toString();
+
 	}
 
 }
